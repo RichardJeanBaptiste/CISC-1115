e34007fe3f124ea82aa71bcc81b1bb5d25f74fbe
Richinbk <Richinbk1@gmail.com>
2019-03-02 20:35:26 -0500

    Update assignment6.java

--- assignment6.java
+++ assignment6.java
@@ -1,28 +1,52 @@
+package assignment6;
 import java.util.*;
 import java.io.*;
-
-public class Assignment6
+/*
+ * Richard Jean-Baptiste
+ * CISC 1115-ETR6 
+ */
+public class assignment6
 {
     public static void main(String[] args)throws IOException
     {
         int size;
-        double[] numbers = {3.0};
+        double[] numbers = {};
+        PrintWriter outFile = new PrintWriter("output.txt");
         
         Scanner in = new Scanner(System.in);
         System.out.println("enter size");
         size = in.nextInt();
 
-
+        
         double[] b = Arrays.copyOf(readData(size, numbers), size);
         
-        System.out.println("------------------------------------\nHere is the original Array\n");
-        printArray(size, b);
-
+        
+        
+        outFile.println("------------------------------------\nHere is the original Array\n");
+        outFile.println(printArray(size, b));
+        
+        
+        
+        double avg = findAverage(size, b);
+        outFile.printf("\n\navg = %.2f\n", avg);
+        
+        
+        double[] second = Arrays.copyOf(howFarAway(size, avg, b), size);
+        
+        outFile.println("------------------------------------\nHere is the second Array\n");
+        outFile.println(printArray(size, second));
+        
+        double avg2 = findAverage(size, second);
+        outFile.printf("\n\navg = %.2f\n", avg2);
+        
+        
+        
+        outFile.close();
         in.close();
         
         
     }
-
+    
     public static double[] readData(int n , double[] numbers)throws IOException
     {
         
@@ -31,7 +55,7 @@ public class Assignment6
         File file = null;
         Scanner infile = null;
 
-        file = new File("test");
+        file = new File("test.txt");
         infile = new Scanner(file);
         
         int index = 0;
@@ -47,58 +71,75 @@ public class Assignment6
 
     }
 
-    public static void printArray(int q, double[] numb)
+    public static String printArray(int q, double[] numb)
     {
-        //System.out.println(Arrays.toString(numb));
-        /*
-            first val
-            lines left
-            index 
-
-            loop through for loop
-
-            everytime index = 5 make a new and keep printing 
-        */
-        int index = 0;
-
+    	
+        int index = -1;
+        String s = "";
+        
 
         for(int i = 0; i < q; i++)
         {
             index++;
             double x = numb[i];
             
-            if(index == 6)
+            if(index == 5)
             {
-                System.out.println("\n");
-                System.out.printf("%.2f ", x);
+                
+                s += String.format("\n%.2f ", x);
                 index = 0;
             }
             else
             {
-                System.out.printf("%.2f ", x);
+                
+            	s += String.format("%.2f ", x);
             }
         }
 
-
+        	return s;
+    }
+    
+    public static double findAverage(int k, double[] p)
+    {
+    	
+    	/* get the sum of k numbers 
+    	 * divide k
+    	 */
+    	
+    	double sum = 0;
+    	double average;
+    	
+    	
+    	for(int i = 0; i <= k - 1; i++)
+    	{
+    		sum += p[i];
+    	}
+    	
+    	average = sum / k;
+    	
+    	
+    	
+    	return average;
+    }
+    
+    public static double[] howFarAway(int m, double avg, double[] r)
+    {
+    	/*
+    	 * loop through all the values in r 
+    	 * subtract avg
+    	 * add result to new array 
+    	 * return array
+    	 */
+    	double[] s = new double[50];
+    	
+    	for(int i = 0; i < r.length; i++)
+    	{
+    		//System.out.println((r[i] - avg));
+    		s[i] = r[i] - avg ;
+    	}
+    	
+    	    	
+    	return s;
     }
   
 }
-
-
-    /*        
-        Scanner infile = new Scanner(new File("test"));
-        ArrayList<Double> fullFile = new ArrayList<Double>();
-
-
-        while (infile.hasNextLine())
-        {
-            //if the number == to double and the length of fullFile == to x
-            if((infile instanceof Double) && (infile.length() <= x) )
-            {
-                fullFile.add(infile.nextDouble());
-            }    
-        }
-
-        //fullFile.forEach(System.out::println);
-        infile.close();
-        */    
